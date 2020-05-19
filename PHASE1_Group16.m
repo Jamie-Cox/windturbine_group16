6b6d6596719c7cb61e6fb0a8c29614c7143d5772
Luís Marques <lfsm01@hotmail.com>
2020-05-19 09:40:52 +0100

    Update PHASE1_Group16.m

--- PHASE1_Group16.m
+++ PHASE1_Group16.m
@@ -51,7 +51,7 @@ line_best_fit_newcgneg6=polyfit(new_cgneg6_cl,new_cgneg6_ih,1);
 
 
 plot(linspace(0,0.7), polyval(line_best_fit_newcg0,linspace(0,0.7)),"r")
-plot(linspace(0,0.7), polyval(line_best_fit_newcgneg6,linspace(0,0.7)),"b")
+plot(linspace(0,0.7), quoi
 scatter(new_cg6_cl,new_cg6_ih,"g")
 plot(linspace(0,0.7), polyval(line_best_fit_newcg6,linspace(0,0.7)),"g")
 legend("cg0: data", "cg0: best fit line","cg-6: data", "cg-6: best fit line","cg6: data", "cg6: best fit line")
